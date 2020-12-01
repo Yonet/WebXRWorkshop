b350988437e3de5f48080d868ace5e929bf2751f
Aysegul Yonet <aysegulyonet@gmail.com>
2020-11-30 23:42:14 -0800

    clean up server

--- src/server/server.ts
+++ src/server/server.ts
@@ -14,62 +14,17 @@ class App {
 		app.use(express.static(path.join(__dirname, "../client")));
 		app.use(
 			"/build/three.module.js",
-			express.static(
+			express.static(path.join(__dirname, "../../node_modules/three/build/three.module.js"))
+		);
+
+		app.use("/jsm/:folder/:file", (req, res) => {
+			res.sendFile(
 				path.join(
 					__dirname,
-					"../../node_modules/three/build/three.module.js"
+					"../../node_modules/three/examples/jsm/" + req.params.folder + "/" + req.params.file + ".js"
 				)
-			)
-		);
-		// app.use('/three/src/cameras/PerspectiveCamera', express.static(path.join(__dirname, '../../node_modules/three/src/cameras/PerspectiveCamera.js')))
-		// app.use('/three/src/renderers/WebGLRenderer', express.static(path.join(__dirname, '../../node_modules/three/src/renderers/WebGLRenderer.js')))
-		// app.use('/three/src/scenes/Scene', express.static(path.join(__dirname, '../../node_modules/three/src/scenes/Scene.js')))
-		// app.use('/three/src/scenes/Scene', express.static(path.join(__dirname, '../../node_modules/three/src/scenes/Scene.js')))
-		app.use(
-			"/jsm/controls/OrbitControls",
-			express.static(
-				path.join(
-					__dirname,
-					"../../node_modules/three/examples/jsm/controls/OrbitControls.js"
-				)
-			)
-		);
-		app.use(
-			"/jsm/libs/stats.module",
-			express.static(
-				path.join(
-					__dirname,
-					"../../node_modules/three/examples/jsm/libs/stats.module.js"
-				)
-			)
-		);
-		app.use(
-			"/jsm/libs/dat.gui.module",
-			express.static(
-				path.join(
-					__dirname,
-					"../../node_modules/three/examples/jsm/libs/dat.gui.module.js"
-				)
-			)
-		);
-		app.use(
-			"/jsm/webxr/ARButton",
-			express.static(
-				path.join(
-					__dirname,
-					"../../node_modules/three/examples/jsm/webxr/ARButton.js"
-				)
-			)
-		);
-		app.use(
-			"/jsm/webxr/VRButton",
-			express.static(
-				path.join(
-					__dirname,
-					"../../node_modules/three/examples/jsm/webxr/VRButton.js"
-				)
-			)
-		);
+			);
+		});
 
 		this.server = new http.Server(app);
 	}
