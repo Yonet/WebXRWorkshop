b30fa77153058dab7af81c1ad1486c3d58485650
Nicholi Noah <x@nicholinoah.com>
2023-01-19 11:48:22 -0600

    port 443

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,43 +1,49 @@
+import fs from "fs";
 import express from "express";
 import path from "path";
-import http from "http";
+import https from "https";
 
-const port: number = 3000;
+const port: number = 443;
 
 class App {
-	private server: http.Server;
-	private port: number;
+    private server: https.Server;
+    private port: number;
 
-	constructor(port: number) {
-		this.port = port;
-		const app = express();
-		app.use(express.static(path.join(__dirname, "../client")));
-		app.use(
-			"/build/three.module.js",
-			express.static(path.join(__dirname, "../../node_modules/three/build/three.module.js"))
-		);
+    constructor( port: number ) {
+        this.port = port;
+        const app = express();
+        app.use( express.static( path.join( __dirname, "../client" ) ) );
+        app.use(
+            "/build/three.module.js",
+            express.static( path.join( __dirname, "../../node_modules/three/build/three.module.js" ) )
+        );
 
-		app.use("/jsm/:folder/:file", (req, res) => {
-			res.sendFile(
-				path.join(
-					__dirname,
-					"../../node_modules/three/examples/jsm/" + req.params.folder + "/" + req.params.file + ".js"
-				)
-			);
-		});
+        app.use( "/jsm/:folder/:file", ( req, res ) => {
+            res.sendFile(
+                path.join(
+                    __dirname,
+                    "../../node_modules/three/examples/jsm/" + req.params.folder + "/" + req.params.file + ".js"
+                )
+            );
+        } );
 
-		app.use("/assets/:folder/:file", (req, res) => {
-			res.sendFile(path.join(__dirname, "../../dist/client/assets/" + req.params.folder + "/" + req.params.file));
-		});
+        app.use( "/assets/:folder/:file", ( req, res ) => {
+            res.sendFile( path.join( __dirname, "../../dist/client/assets/" + req.params.folder + "/" + req.params.file ) );
+        } );
 
-		this.server = new http.Server(app);
-	}
+        const options = {
+            key: fs.readFileSync( 'c:/GitRepos/localhost-key.pem' ),
+            cert: fs.readFileSync( 'c:/GitRepos/localhost.pem' )
+        };
+        this.server = https.createServer( options, app );
 
-	public Start() {
-		this.server.listen(this.port, () => {
-			console.log(`Server listening on port ${this.port}.`);
-		});
-	}
+    }
+
+    public Start () {
+        this.server.listen( this.port, () => {
+            console.log( `Server listening on port ${ this.port }.` );
+        } );
+    }
 }
 
-new App(port).Start();
+new App( port ).Start();
